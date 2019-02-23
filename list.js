6638d609bd7762ed7c5cfb0c85d9cc0e5f5ff674
viveksuarez7 <46070223+viveksuarez7@users.noreply.github.com>
2019-02-24 00:14:27 +0530

    Add files via upload

---
+++ list.js
@@ -0,0 +1,17 @@
+var todos=[];
+var input =prompt("What would you like to do ?")
+
+while (input !=="quit"){
+	if(input === "list"){
+	console.log(todos);
+}
+else if(input === "new"){
+	//ask for new todo
+	var newTodo = prompt("Enter new todo");
+	//add to todos array
+	todos.push(newTodo);
+}
+//ask for new input
+input=prompt("What would you like to do")
+}
+console.log("Ok you quit the app");
